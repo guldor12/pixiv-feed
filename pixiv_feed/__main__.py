f32d409f91ced2dfae9e483b9251ddc8d8f2800e
guldor12 <>
2022-08-06 01:27:35 -0500

    Add login/refresh commands

--- pixiv_feed/__main__.py
+++ pixiv_feed/__main__.py
@@ -2,6 +2,7 @@ import sys
 import click
 from . import MyAppPixivAPI, select_feed
 from .flask import app
+from .auth import login as pixiv_login
 
 
 @click.group(add_help_option=False)
@@ -48,6 +49,56 @@ def generate(feedtype, endpoint, language):
 @click.option("-p", "--port", type=int, help="Port to serve feeds on")
 def server(host, port):
     app.run(host=host, port=port)
+
+    return 0
+
+
+# fmt: off
+@cli.command(add_help_option=False, short_help="Obtain authentication tokens", help="""
+    1. Run the command. This will open the browser with Pixiv login page.
+
+    2. Open dev console (F12) and switch to network tab.
+
+    3. Enable persistent logging ("Preserve log").
+
+    4. Type into the filter field: callback?
+
+    5. Proceed with Pixiv login.
+
+    \b
+    6. After logging in you should see a blank page and request that looks like this:
+
+    \b
+       https://app-api.pixiv.net/web/v1/users/auth/pixiv/callback?state=...&code=....
+
+       Copy value of the code param into the pixiv_auth.py's prompt and hit the Enter key.
+
+    If you did everything right and Pixiv did not change their auth flow, pair of auth_token and refresh_token should be displayed.
+
+    \b
+    WARNING:
+    The lifetime of code is extremely short, so make sure to minimize delay
+    between step 5 and 6. Otherwise, repeat everything starting step 1.
+"""
+)
+@click.help_option("-h", "--help")
+# fmt: on
+def login():
+    auth_data = pixiv_login()
+    pixiv = MyAppPixivAPI()
+    pixiv.set_auth(auth_data["access_token"], auth_data["refresh_token"])
+    pixiv.refresh()
+
+    return 0
+
+
+@cli.command(add_help_option=False, help="Add authentication tokens")
+@click.help_option("-h", "--help")
+@click.argument("refresh_token")
+def refresh(refresh_token):
+    pixiv = MyAppPixivAPI()
+    pixiv.refresh_token = refresh_token
+    pixiv.refresh()
 
     return 0
 
---
+++ pixiv_feed/auth.py
@@ -0,0 +1,71 @@
+from base64 import urlsafe_b64encode
+from hashlib import sha256
+from pprint import pprint
+from secrets import token_urlsafe
+from sys import exit
+from urllib.parse import urlencode
+from webbrowser import open as open_url
+
+import requests
+
+# Latest app version can be found using GET /v1/application-info/android
+USER_AGENT = "PixivAndroidApp/5.0.234 (Android 11; Pixel 5)"
+REDIRECT_URI = "https://app-api.pixiv.net/web/v1/users/auth/pixiv/callback"
+LOGIN_URL = "https://app-api.pixiv.net/web/v1/login"
+AUTH_TOKEN_URL = "https://oauth.secure.pixiv.net/auth/token"
+CLIENT_ID = "MOBrBDS8blbauoSck0ZfDbtuzpyT"
+CLIENT_SECRET = "lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj"
+
+
+def s256(data):
+    """S256 transformation method."""
+
+    return urlsafe_b64encode(sha256(data).digest()).rstrip(b"=").decode("ascii")
+
+
+def oauth_pkce(transform):
+    """Proof Key for Code Exchange by OAuth Public Clients (RFC7636)."""
+
+    code_verifier = token_urlsafe(32)
+    code_challenge = transform(code_verifier.encode("ascii"))
+
+    return code_verifier, code_challenge
+
+
+def login():
+    code_verifier, code_challenge = oauth_pkce(s256)
+    login_params = {
+        "code_challenge": code_challenge,
+        "code_challenge_method": "S256",
+        "client": "pixiv-android",
+    }
+
+    open_url(f"{LOGIN_URL}?{urlencode(login_params)}")
+
+    try:
+        code = input("code: ").strip()
+    except (EOFError, KeyboardInterrupt):
+        return
+
+    response = requests.post(
+        AUTH_TOKEN_URL,
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "code": code,
+            "code_verifier": code_verifier,
+            "grant_type": "authorization_code",
+            "include_policy": "true",
+            "redirect_uri": REDIRECT_URI,
+        },
+        headers={"User-Agent": USER_AGENT},
+    )
+
+    data = response.json()
+
+    if "access_token" in data and "refresh_token" in data:
+        return data
+
+    print("error:")
+    pprint(data)
+    exit(1)
